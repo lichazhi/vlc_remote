f68635eaea8364bd71f3529864acc8727d4b9a28
Neil <neil.y.yeung@gmail.com>
2017-08-04 13:28:35 -0700

    can play, hardcoded to play first item in listView, also changed to portrait in manifest

---
+++ app/src/main/java/genuinely/vlc_remote/Status.java
@@ -0,0 +1,7 @@
+package genuinely.vlc_remote;
+
+/**
+ * Created by neilyeung on 8/4/17.
+ */
+public class Status {
+}
---
+++ app/src/main/java/genuinely/vlc_remote/dummy/StatusController.java
@@ -0,0 +1,7 @@
+package genuinely.vlc_remote.dummy;
+
+/**
+ * Created by neilyeung on 8/3/17.
+ */
+public class StatusController {
+}
